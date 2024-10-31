b30fce2c3940cf3cf8b5260aff056e7d4ebcff95
Hiroki Fujiwara <hafnium49@gmail.com>
2024-10-31 11:06:33 +0900

    update add_tasks_prompt

--- chemistry_3d_mas.py
+++ chemistry_3d_mas.py
@@ -164,9 +164,9 @@ class Chemistry3DMAS(BaseSample):
         self.mas = MAS(world, self.controller_manager)
 
         # Perform simulation updates
-        self.Sim_Beaker_Kmno4.sim_update(self.Sim_Bottle_Kmno4, self.Franka, self.controller_manager)
-        self.Sim_Beaker_Fecl2.sim_update(self.Sim_Bottle_Fecl2, self.Franka, self.controller_manager)
-        self.Sim_Beaker_Fecl2.sim_update(self.Sim_Beaker_Kmno4, self.Franka, self.controller_manager)
+        # self.Sim_Beaker_Kmno4.sim_update(self.Sim_Bottle_Kmno4, self.Franka, self.controller_manager)
+        # self.Sim_Beaker_Fecl2.sim_update(self.Sim_Bottle_Fecl2, self.Franka, self.controller_manager)
+        # self.Sim_Beaker_Fecl2.sim_update(self.Sim_Beaker_Kmno4, self.Franka, self.controller_manager)
 
         # Re-initialize variables
         self.user_prompt = None
