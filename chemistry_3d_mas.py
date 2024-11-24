1400065549032e09375fe9f5c099521a72f19639
Hiroki Fujiwara <hafnium49@gmail.com>
2024-11-24 15:14:12 +0900

    undo last debug

--- chemistry_3d_mas.py
+++ chemistry_3d_mas.py
@@ -327,8 +327,8 @@ class Chemistry3DMAS(BaseSample):
     def sim_step(self, step_size):
         world = self.get_world()
         if world.is_playing():
-            # Check if the robot's simulation view is ready
-            if not self.Franka.is_simulation_ready():
+            # Added check to ensure physics simulation view is created
+            if not self.Franka.is_simulation_view_created():
                 # Skip this step until the simulation view is ready
                 return
 
